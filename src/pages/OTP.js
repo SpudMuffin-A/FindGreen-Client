2da23e81e3d5f99090254759813f3bdcfada2889
Aryan Vaidya <55594646+SpudMuffin-A@users.noreply.github.com>
2023-05-14 12:29:07 +0530

    Update OTP.js

--- src/pages/OTP.js
+++ src/pages/OTP.js
@@ -8,14 +8,14 @@ function Otp() {
       <div className="md:w-1/2">
         <img
           aria-hidden="true"
-          className="object-cover w-full h-screen dark:hidden"
+          className="object-cover w-full h-screen"
           src={ImageLight}
           alt="Office"
         />
       </div>
       <main className="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
         <div className="w-full">
-          <h1 className="mb-4 text-3xl font-semibold text-gray-700 dark:text-gray-200">
+          <h1 className="mb-4 text-3xl font-semibold text-gray-700">
             Enter OTP
           </h1>
           <OtpForm />
